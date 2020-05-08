240906f81812ac1dc8e150738325f5a3aba42eb9
TomasKomprs <tomas.komprs@gmail.com>
2020-05-08 15:44:05 +0200

    Add data/error validation for users saga, to prevent app crash

--- src/app/components/users/sagas/fetchUsersSaga.ts
+++ src/app/components/users/sagas/fetchUsersSaga.ts
@@ -3,11 +3,17 @@ import { USERS_API } from '../../../../http/api';
 import { SET_USERS_DATA, SET_USERS_ERROR, SET_USERS_LOADING } from '../constants';
 
 export function* fetchUsersSaga() {
+  yield put({ type: SET_USERS_ERROR, data: false });
   yield put({ type: SET_USERS_LOADING, data: true });
 
   try {
     const response = yield fetch(USERS_API);
     const result = yield response.json();
+
+    if (!Array.isArray(result)) {
+      yield put({ type: SET_USERS_DATA, data: [] });
+      throw new Error('');
+    }
     yield put({ type: SET_USERS_DATA, data: result });
   } catch (e) {
     yield put({ type: SET_USERS_ERROR, data: true });
